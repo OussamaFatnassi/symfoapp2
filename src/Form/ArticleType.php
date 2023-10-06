e88e7ca54f5fdee2b38c9a6ca33f804e7f90a6ce
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2023-10-06 12:47:41 +0200

    fix(article): update form article

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,15 +6,33 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('enable');
+            ->add('title', TextType::class, [
+                'label' => 'Titre:',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Titre de votre article"
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu:',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Contenu de votre article',
+                    'rows' => 5,
+                ]
+            ])
+            ->add('enable', CheckboxType::class, [
+                'label' => 'Actif',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
